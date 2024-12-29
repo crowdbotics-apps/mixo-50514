c63b5ed456cc8ebb8f30280f7c3636b75aa36ec5
Crowdbotics <team@crowdbotics.com>
2024-12-29 12:03:48 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mixo-50514",
+  url: "https://mixo-50514.botics.co",
+  api: "https://mixo-50514.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
